9646d1f96a6e501a521c6b2605cdf0104bb83904
Stanislav Mikhaylov <ceo@adflare.ru>
2016-01-14 15:05:58 +0300

    Fixed scipts. Added Partials

--- routes/app.js
+++ routes/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/login/', function(req, res, next) {
-    res.render('login', { title: 'login' });
+    res.render('app/login', { title: 'Вход' });
 });
 
 module.exports = router;
